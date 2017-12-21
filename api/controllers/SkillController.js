43580e9632ba88dd5c9e7df0b08cbb59e450bd27
neos <nvdnvd00@gmail.com>
2017-12-21 19:01:53 +0700

    aaa

--- api/controllers/SkillController.js
+++ api/controllers/SkillController.js
@@ -8,11 +8,13 @@
 module.exports = {
 	get_skill:function (req,res){
         var typeSkill = req.param("typeSkill");
-        Skill.query('SELECT a.id,a.name,b.name as lv,a.level,a.url FROM `skill` a, `level` b WHERE a.level=b.id AND a.class=? ',[typeSkill],function (err,skill) 
+       // var num = req.param("num");
+        Skill.query('SELECT a.id,a.name,b.name as lv,a.level,a.url FROM `skill` a, `level` b WHERE a.level=b.id AND a.class=?',[typeSkill],function (err,skill) 
         {
             if(err){
             return res.json(err);
           }
+
           return res.json(skill);
             
         })
